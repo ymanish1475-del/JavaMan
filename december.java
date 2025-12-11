32323b1021a0a4eb434d59450cc5a9df6dd8a188
ymanish1475-del <ymanish1475@gmail.com>
2025-12-11 18:58:17 +0530

    my java for multithreading

---
+++ december.java
@@ -0,0 +1,9 @@
+public class december {
+    int balance =100;
+    void withdraw ( int amt ){
+        System.out.println(Thread.currentThread().getName()+ "%");
+        if(balance >= amt){
+            
+        }
+    }
+}
