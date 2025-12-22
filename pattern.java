6d862de0b32e476e74428e4a05d950b41c1db265
ymanish1475-del <ymanish1475@gmail.com>
2025-12-22 22:14:38 +0530

    my java learning

---
+++ pattern.java
@@ -0,0 +1,36 @@
+public class pattern {
+    
+}
+// import java.util.*;
+// public class lec2 {
+//     public static void main(String [] args){
+//         // String name = "tony stark";
+//         // int age = 48;
+//         // double price = 99.99;
+//         // int a =25;
+//         // int b = 10;
+//         // b=100;
+//         // System.out.println(name);
+//         //   System.out.println(age);
+//         //     System.out.println(a);
+//         //       System.out.println(b);
+//         // int a=10;
+//         // int b = 25;
+//         // int sum= a+b;
+//         // int mul=a*b;
+//         // float sol = mul/sum;
+//         // float  ans = (a*b)/(a+b);
+//         // float answer = a*b/a+b;
+//         // System.out.println(sum);
+//         // System.out.println(mul);
+//         // System.out.println(sol);
+//         // System.out.println(ans);
+//         // System.out.println(answer);
+//         Scanner one =new Scanner(System.in);
+//         String name =one.next();
+//         System.out.println(name);
+
+
+
+//     }
+// }
