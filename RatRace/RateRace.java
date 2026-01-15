08e3d0f184b296c0c6129cbbec4393c74c278c10
ymanish1475-del <ymanish1475@gmail.com>
2026-01-15 18:53:12 +0530

    PROBLEM SOLUTION

---
+++ RatRace/RateRace.java
@@ -0,0 +1,14 @@
+package RatRace;
+
+public class RateRace {
+    public  static void  main (String  args[]){
+
+        int [][] maze = {
+            {1,0,0,0}
+            {1,0,0,0}
+            {1,1,1,0}
+            {1,0,1,1}
+        }
+
+    }
+}
