36ed5636b51f9fca77121b5aa607b7156df51c45
ymanish1475-del <ymanish1475@gmail.com>
2026-01-19 19:47:11 +0530

    linledlist

---
+++ likedList/new.java
@@ -0,0 +1,24 @@
+package likedList;
+
+class node{
+    int data;
+    Node next;
+
+    public Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
+class MyLinkedList{
+    Node head;
+    void insertAtBegining(int data){
+        Node
+    }
+}
+
+public class new {
+    public static void main(String args[]){
+
+    }
+    
+}
